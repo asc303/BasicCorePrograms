7f883dcf3f2184431f16cd687a84492c1bd5a4b1
asc303 <40396449+asc303@users.noreply.github.com>
2021-11-12 16:24:41 +0530

    Update Program.cs

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace BasicCorePrograms
 {
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. Exit");
+                Console.WriteLine("Enter the Program number which is to be executed \n 1. Flip Coin \n 2. Leap Year \n 3. Power of Two \n 4. Harmonic Number \n 5. Factors \n 6. Quotient Remainder \n 7. SwapTwoNumers \n 8. Even Or Odd Number \n 9. Vowel or Consonent \n 10.Largest Number \n 11.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -36,6 +36,22 @@ namespace BasicCorePrograms
                         result.Division();
                         break;
                     case 7:
+                        Swapping ans = new Swapping();
+                        ans.Swap();
+                        break;
+                    case 8:
+                        EvenOrOdd num = new EvenOrOdd();
+                        num.EvenOdd();
+                        break;
+                    case 9:
+                        VowelorConsonant letter = new VowelorConsonant();
+                        letter.Alphabet();
+                        break;
+                    case 10:
+                        LargestNumber max = new LargestNumber();
+                        max.Largest();
+                        break;
+                    case 11:
                         flag = false;
                         break;
                 }
