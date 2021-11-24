c14e80dc18d2117607d6f8ffd398eda131118cb2
Ashutosh Singh <40396449+asc303@users.noreply.github.com>
2021-11-24 22:54:07 +0530

    Update Factors.cs

--- BasicCorePrograms/BasicCorePrograms/Factors.cs
+++ BasicCorePrograms/BasicCorePrograms/Factors.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,24 @@ namespace BasicCorePrograms
 {
     class Factors
     {
+        public void Factor()
+        {
+            Console.WriteLine("Enter the Number to Calculate is Factors :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (int i = 2; i*i <= number; i++)
+            {
+                while (number % i == 0)                
+                {
+                    Console.WriteLine(i);
+                    number /= i;
+                }
+            }
+            if (number >1)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Those are the Factors ");
+        }
     }
 }
